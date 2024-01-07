c0068042d9e2ea182673a74873c59827c2bd5b31
KawaiiNee <kasuganosora002@gmail.com>
2024-01-07 20:20:06 +0800

    initial commit

---
+++ src/pages/content.js
@@ -0,0 +1,167 @@
+// Personal Information --> #hero section
+export const name = "Jaymart Padilla";
+export const designation = "Full Stack Web Developer";
+export const email = "jaymartpadilla.dev@gmail.com";
+export const pronouns = "he/him";
+export const linkedIn = "https://www.linkedin.com/in/jaymartpadilla/";
+export const website = "https://www.linkedin.com/in/jaymartpadilla/";
+
+// About
+export const about =
+  "My name is Jaymart R. Padilla, This is a placeholder text for the about section.";
+
+// Work Experience --> #work section
+export const work = [
+  {
+    from: "2001",
+    to: "Present",
+    role: "Safety Inspector",
+    org: "Quahog Safety Department",
+    org_url: "https://quahog.gov/safety",
+    location: "Quahog, Rhode Island",
+    skills: "Safety Inspection, Risk Assessment, Emergency Response",
+  },
+];
+
+// Projects --> #project section
+export const projects = [
+  {
+    title: "AlumTrack",
+    description:
+      "AlumTrack is an advanced alumni tracking system with integrated data analytics, exclusively tailored for the Pangasinan State University Alaminos City Campus.",
+    url: "https://github.com/jaymart-padilla/AlumTrack",
+    tech: "Alumni Tracking, Data Analytics, Web Development",
+  },
+  {
+    title: "SitOnMe",
+    description:
+      "E-commerce web app built for imaginary furniture e-commerce company, primarily focusing on chairs.",
+    url: "https://github.com/jaymart-padilla/SitOnMe",
+    tech: "E-commerce, Web Development",
+  },
+  {
+    title: "Touhou Project",
+    description:
+      "A simple pixel game inspired by the classic Japanese game Touhou Project.",
+    url: "https://github.com/jaymart-padilla/touhou-project",
+    tech: "Pixel Art Game, Game Development",
+  },
+  {
+    title: "Student Registration and Admission System",
+    description:
+      "This project was developed for my college subject (WS 101). It simulates the complete process of online registration and admission. It provides a comprehensive solution to streamline and manage the student enrollment journey.",
+    url: "https://github.com/jaymart-padilla/student-registration-and-admission-system",
+    tech: "Web Development, Database Management",
+  },
+];
+
+// Education --> #education section
+export const education = [
+  {
+    from: "June, 2018",
+    to: "April, 2020",
+    institute: "Alaminos City National High School",
+    url: "https://www.google.com/maps/place/Alaminos+City+National+High+School/@16.1665129,119.9866996,15z/data=!4m6!3m5!1s0x3393dcba1fd1425f:0x62886852fc0f73f4!8m2!3d16.1665129!4d119.9866996!16s%2Fg%2F113dwrcc_?entry=ttu",
+    course: "General Academic Strand",
+    location: "5X8P+JM4, Magsaysay, Alaminos, Pangasinan",
+  },
+  {
+    from: "2020",
+    to: "2024",
+    institute: "Pangasinan State University - Alaminos City Campus",
+    url: "https://alaminos.psu.edu.ph/",
+    course: "Bachelor of Science in Information Technology",
+    location: "Zone 3, 4X8C+52X, Alaminos, Pangasinan",
+  },
+];
+
+// Contact --> #contact section
+export const contact = [
+  {
+    source_name: "Email | jaymartpadilla.dev@gmail.com",
+    source: "#",
+  },
+  {
+    source_name: "Phone | +639519523387",
+    source: "#",
+  },
+  {
+    source_name: "LinkedIn",
+    source: "https://www.linkedin.com/in/jaymartpadilla/",
+  },
+];
+
+// Certifications --> #certificate section
+
+export const certificates = [
+  {
+    issued: "2005",
+    title: "Certified Safety Inspector",
+    org: "National Safety Association",
+    url: "https://certi.com/url",
+    description: "Certification in safety inspection and risk assessment.",
+  },
+  {
+    issued: "2006",
+    title: "Emergency Response Training",
+    org: "Quahog Safety Department",
+    url: "https://certi.com/url",
+    description:
+      "Training in handling emergency situations and crisis management.",
+  },
+  {
+    issued: "2007",
+    title: "Safety Leadership Workshop",
+    org: "Safety Leaders International",
+    url: "https://certi.com/url",
+    description: "Leadership training in safety management and communication.",
+  },
+];
+
+// Blogs --> #blogs section
+// export const blogs = [
+//   {
+//     date: "12-July, 2023",
+//     url: "https://medium.com/",
+//     title: "Adventures in Safety Inspection",
+//     description:
+//       "Join me in my humorous escapades as a safety inspector in Quahog.",
+//     publisher: "Quahog Times",
+//   },
+//   {
+//     date: "25-June, 2023",
+//     url: "https://medium.com/",
+//     title: "Top 10 Safety Tips for Families",
+//     description:
+//       "Learn the best safety practices for families to ensure a secure environment.",
+//     publisher: "Safety Monthly",
+//   },
+// ];
+
+// Achievements --> #achievement section
+export const achievements = [
+  {
+    title: "Safety Hero Award",
+    year: "2020",
+    decription:
+      "Recognized for outstanding contributions to improving safety standards in Quahog.",
+  },
+  {
+    title: "Safety Innovation Prize",
+    year: "2018",
+    decription:
+      "Awarded for implementing innovative safety measures that yielded significant improvements.",
+  },
+  {
+    title: "Community Safety Ambassador",
+    year: "2015",
+    decription:
+      "Honored for promoting safety awareness and education in the Quahog community.",
+  },
+  {
+    title: "Safety Campaign Excellence",
+    year: "2013",
+    decription:
+      "Acknowledged for leading a successful safety campaign that reached thousands of residents.",
+  },
+];
